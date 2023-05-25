5db36f8196b32cfb172510cfd0f2a285dd6085e2
sonb9615 <sonb9615@gmail.com>
2023-05-25 12:29:40 +0900

    [23.05.25] init web-novel

---
+++ web-novel/src/main/java/numbel/webnovel/WebNovelApplication.java
@@ -0,0 +1,13 @@
+package numbel.webnovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebNovelApplication.class, args);
+    }
+
+}
---
+++ web-novel/src/test/java/numbel/webnovel/WebNovelApplicationTests.java
@@ -0,0 +1,13 @@
+package numbel.webnovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNovelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
